8039199c8b2b9769a9c66046e3c3b781b2bc2b8b
Drojecki <127322846+Drojecki@users.noreply.github.com>
2024-08-17 14:31:42 +0200

    Update db.js

--- inzynierka/src/backend/config/db.js
+++ inzynierka/src/backend/config/db.js
@@ -1,19 +1,1 @@
-// config/db.js
-const mysql = require('mysql2');
 
-const db = mysql.createConnection({
-  host: '34.116.229.68',
-  user: 'root',
-  password: 'QN|9YDTy[Tex3,04',
-  database: 'inzynierka'
-});
-
-db.connect(err => {
-  if (err) {
-    console.error('Błąd połączenia z bazą danych:', err);
-    return;
-  }
-  console.log('Połączono z bazą danych MySQL');
-});
-
-module.exports = db;
