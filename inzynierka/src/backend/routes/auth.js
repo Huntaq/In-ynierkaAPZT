e89c4d64cc51fcb32df4957d8509ff5c1b0e34ab
Drojecki <127322846+Drojecki@users.noreply.github.com>
2024-11-02 20:21:08 +0100

    tailwind

--- inzynierka/src/backend/routes/auth.js
+++ inzynierka/src/backend/routes/auth.js
@@ -20,7 +20,7 @@ const authenticateToken = (req, res, next) => {
     db.query('SELECT session_key FROM users WHERE id = ?', [payload.id], (err, results) => {
       if (err) {
         console.error('db error:', err);
-        return res.status(500).json({ error: 'querry error' });
+        return res.status(500).json({ error: 'error' });
       }
 
       if (results.length === 0 || results[0].session_key !== payload.sessionKey) {
@@ -37,49 +37,69 @@ const authenticateToken = (req, res, next) => {
 
 router.post('/register', async (req, res) => {
   const { name, password, email, age, gender } = req.body;
-  console.log('Received data:', { name, password, email, age, gender });
-  
+
   if (!name || !password || !email || !age || !gender) {
-    return res.status(400).json({ error: 'All fields are required' });
+    return res.status(400).json({ error: 'missing field' });
   }
 
   try {
-    const hashedPassword = await bcrypt.hash(password, 10);
-
-    const userData = {
-      name,
-      password: hashedPassword,
-      email,
-      age,
-      gender,
-      created_at: moment().format('YYYY-MM-DD HH:mm:ss')
-    };
-
-    const query = `
-      INSERT INTO users (username, password_hash, email, age, gender, created_at)
-      VALUES (?, ?, ?, ?, ?, ?)
+    const checkQuery = `
+      SELECT * FROM users WHERE username = ? OR email = ?
     `;
+    const checkValues = [name, email];
 
-    const values = [
-      userData.name,
-      userData.password,
-      userData.email,
-      userData.age,
-      userData.gender,
-      userData.created_at
-    ];
-
-    db.query(query, values, (err, results) => {
+    db.query(checkQuery, checkValues, async (err, results) => { 
       if (err) {
-        console.error('querry error:', err);
+        console.error('DB error', err);
         return res.status(500).json({ error: 'DB error' });
       }
-      res.json({ message: 'User registered successfully!' });
-      console.log('udalo sie');
+
+      if (results.length > 0) {
+        const existingUser = results[0];
+        if (existingUser.username === name) {
+            return res.status(400).json({ error: 'username exists' });
+        }
+        if (existingUser.email === email) {
+            return res.status(400).json({ error: 'email exists' });
+        }
+    }
+
+      const hashedPassword = await bcrypt.hash(password, 10);
+
+      const userData = {
+        name,
+        password: hashedPassword,
+        email,
+        age,
+        gender,
+        created_at: moment().format('YYYY-MM-DD HH:mm:ss')
+      };
+
+      const query = `
+        INSERT INTO users (username, password_hash, email, age, gender, created_at)
+        VALUES (?, ?, ?, ?, ?, ?)
+      `;
+
+      const values = [
+        userData.name,
+        userData.password,
+        userData.email,
+        userData.age,
+        userData.gender,
+        userData.created_at
+      ];
+
+      db.query(query, values, (err, results) => {
+        if (err) {
+          console.error('DB error', err);
+          return res.status(500).json({ error: 'DB error' });
+        }
+        res.json({ message: 'success' });
+      });
     });
   } catch (error) {
-    console.error('Error:', error);
-    res.status(500).json({ error: 'Server error' });
+    console.error('error', error);
+    res.status(500).json({ error: 'error' });
   }
 });
 
@@ -87,18 +107,17 @@ router.post('/login', async (req, res) => {
   const { username, password } = req.body;
 
   if (!username || !password) {
-    return res.status(400).json({ error: 'Username and pass are required' });
+    return res.status(400).json('Username and pass are required');
   }
 
   try {
     db.query('SELECT * FROM users WHERE username = ?', [username], async (err, results) => {
       if (err) {
-        console.error('DB error:', err);
-        return res.status(500).json({ error: 'querry error' });
+        return res.status(500);
       }
 
       if (results.length === 0) {
-        return res.status(400).json({ error: 'Wrong username or pass' });
+        return res.status(400);
       }
 
       const user = results[0];
@@ -109,20 +128,18 @@ router.post('/login', async (req, res) => {
         
         db.query('UPDATE users SET session_key = ? WHERE id = ?', [sessionKey, user.id], (err) => {
           if (err) {
-            console.error('Błąd aktualizacji klucza sesji:', err);
-            return res.status(500).json({ error: 'Błąd aktualizacji klucza sesji' });
+            return res.status(500);
           }
           
           const token = jwt.sign({ id: user.id, username: user.username,Admin: user.is_Admin, sessionKey }, SECRET_KEY, { expiresIn: '12h' });
           res.json({ token });
         });
       } else {
-        return res.status(400).json({ error: 'Wrong username or pass' });
+        return res.status(400);
       }
     });
   } catch (error) {
-    console.error('server error:', error);
-    res.status(500).json({ error: 'server error' });
+    res.status(500);
   }
 });
 
--- inzynierka/src/frontend/Register.js
+++ inzynierka/src/frontend/Register.js
@@ -1,7 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import '../css/register.css';
-
 const Register = () => {
     const [name, setName] = useState('');
     const [password, setPassword] = useState('');
@@ -9,14 +7,13 @@ const Register = () => {
     const [age, setAge] = useState('');
     const [gender, setGender] = useState('');
     const [showPassword, setShowPassword] = useState(false);
-
     const [nameError, setNameError] = useState('');
     const [passwordError, setPasswordError] = useState('');
     const [emailError, setEmailError] = useState('');
     const [ageError, setAgeError] = useState('');
     const [genderError, setGenderError] = useState('');
-
-    const [successMessage, setSuccessMessage] = useState(''); 
+    const [errorMessageDuplicate, setErrorMessageDuplicate] = useState('');
+    const [successMessage, setSuccessMessage] = useState('');
 
     const validateName = (name) => /^[A-Za-z]{2,30}$/.test(name);
     const validatePassword = (password) => {
@@ -57,6 +54,7 @@ const Register = () => {
     }, [navigate]);
 
     const handleRegister = () => {
+        setErrorMessageDuplicate('');
         let isValid = true;
 
         if (!validateName(name)) {
@@ -112,102 +110,128 @@ const Register = () => {
             },
             body: JSON.stringify(userData),
         })
-        .then(response => response.json())
-        .then(data => {
-            console.log('Success:', data);
-            
-            setSuccessMessage('Rejestracja udana');
-            
-            setTimeout(() => {
-                navigate('/');
-            }, 3000);
-        })
-        .catch(error => {
-            console.error('Error:', error);
-        });
+            .then(response => {
+                if (!response.ok) {
+                    return response.json().then(errorData => {
+                        if (errorData.error === 'username exists') {
+                            setErrorMessageDuplicate('This nickname is already in use');
+                        }
+                        else if (errorData.error === 'email exists') {
+                            setErrorMessageDuplicate('This email is already in use');
+                        }
+                        return Promise.reject();
+                    });
+                }
+                return response.json();
+            })
+            .then(data => {
+                console.log('Success:', data);
+                setSuccessMessage('Rejestracja udana');
+
+                setTimeout(() => {
+                    navigate('/');
+                }, 3000);
+            })
+            .catch(error => {
+                console.error('Error:', error);
+            });
     };
 
     return (
-        <div className='container2'>
-            <div className='row2 marginP'>
-                <h2 className='register'>Register</h2>
+        <div className='flex flex-col items-center justify-center w-full h-screen p-6 bg-gray-100'>
+            {errorMessageDuplicate && <p className="text-red-500">{errorMessageDuplicate}</p>}
+
+            <div className='mb-4'>
+                <h2 className='text-2xl font-bold text-gray-700'>Register</h2>
+                {successMessage && <p className='text-green-500'>{successMessage}</p>}
             </div>
 
-            
-            {successMessage && (
-                <div className='row2 success-message'>
-                    <p>{successMessage}</p>
-                </div>
-            )}
-
-            <div className='row2'>
-                <p className='inline1 inputype1'>Nickname </p>
-                {nameError && <p className='error inline1'>{nameError}</p>}
-                <input
-                    className='inputRegister margin-top'
-                    value={name}
-                    onChange={(e) => setName(e.target.value)}
-                />
-            </div>
-            <div className='row2'>
-                <p className='inline1 inputype1'>Password </p>
-                {passwordError && <p className='error1 inline1'>{passwordError}</p>}
-                <div className='input-container'>
+            <div className='w-full max-w-xs'>
+                <div className='mb-4'>
+                    <p className='text-gray-600'>Nickname</p>
+                    {nameError && <p className='text-red-500 '>{nameError}</p>}
                     <input
-                        type={showPassword ? 'text' : 'password'}
-                        className='inputRegister margin-top'
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
+                        className='max-w-[300px] w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500'
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                        placeholder='Enter your nickname'
                     />
-                    <button
-                        type='button'
-                        className='show-password-button'
-                        onClick={() => setShowPassword(!showPassword)}
+                </div>
+
+                <div className='mb-4'>
+                    <div className='flex text-sm'>
+                        <p className='text-gray-600'>Password</p>
+                        {passwordError && <p className='text-red-500'>{passwordError}</p>}
+                    </div>
+                    <div className='flex items-center'>
+                        <input
+                            type={showPassword ? 'text' : 'password'}
+                            className='max-w-[300px] w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 w-full'
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
+                        <button
+                            type='button'
+                            className='ml-2 text-blue-500 hover:underline'
+                            onClick={() => setShowPassword(!showPassword)}
+                        >
+                            {showPassword ? 'Hide' : 'Show'}
+                        </button>
+                    </div>
+                </div>
+
+                <div className='mb-4'>
+                    <p className='text-gray-600'>E-mail</p>
+                    {emailError && <p className='text-red-500'>{emailError}</p>}
+                    <input
+                        type='email'
+                        className='max-w-[300px] w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500'
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        placeholder='Enter your email'
+                    />
+                </div>
+
+                <div className='mb-4'>
+                    <p className='text-gray-600'>Age</p>
+                    {ageError && <p className='text-red-500'>{ageError}</p>}
+                    <input
+                        type='number'
+                        className='max-w-[300px] w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500'
+                        value={age}
+                        onChange={(e) => setAge(e.target.value)}
+                        placeholder='Enter your age'
+                    />
+                </div>
+
+                <div className='mb-4'>
+                    <p className='text-gray-600'>Gender</p>
+                    {genderError && <p className='text-red-500'>{genderError}</p>}
+                    <select
+                        className='p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500'
+                        value={gender}
+                        onChange={(e) => setGender(e.target.value)}
                     >
-                        {showPassword ? 'Hide' : 'Show'}
+                        <option value=''>Select Gender</option>
+                        <option value='M'>Male</option>
+                        <option value='F'>Female</option>
+                    </select>
+                </div>
+
+                <div className='flex items-center justify-between'>
+                    <button
+                        className='w-full p-2 bg-gray-300 text-gray-700 rounded-md hover:bg-gray-400 focus:outline-none focus:ring-2 focus:ring-gray-300'
+                        onClick={() => window.location.href = '../'}
+                    >
+                        Go back
+                    </button>
+                    <button
+                        className='w-full p-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-300 ml-2'
+                        onClick={handleRegister}
+                    >
+                        Register
                     </button>
                 </div>
-            </div>
-            <div className='row2'>
-                <p className='inline1 inputype1'>E-mail </p>
-                {emailError && <p className='error inline1'>{emailError}</p>}
-                <input
-                    type='email'
-                    className='inputRegister margin-top'
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                />
-            </div>
-            <div className='row2'>
-                <p className='inline1 inputype1'>Age </p>
-                {ageError && <p className='error inline1'>{ageError}</p>}
-                <input
-                    type='number'
-                    className='inputRegister margin-top'
-                    value={age}
-                    onChange={(e) => setAge(e.target.value)}
-                />
-            </div>
-            <div className='row2'>
-                <p className='inline1 inputype1'>Gender </p>
-                {genderError && <p className='error inline1'>{genderError}</p>}
-                <select
-                    className='inputRegister margin-top' 
-                    value={gender}
-                    onChange={(e) => setGender(e.target.value)}
-                >
-                    <option value=''>Select Gender</option>
-                    <option value='M'>Male</option>
-                    <option value='F'>Female</option>
-                </select>
-            </div>
-            <div className='row2'>
-                <button className='button margin-top inline1' onClick={() => window.location.href = '../'}>
-                    Go back
-                </button>
-                <button className='button margin-top float-right inline1' onClick={handleRegister}>
-                    Register
-                </button>
             </div>
         </div>
     );
