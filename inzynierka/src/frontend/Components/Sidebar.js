276061659f0bc69104c5b7515ef2daee67a1568a
Drojecki <127322846+Drojecki@users.noreply.github.com>
2024-11-03 21:12:46 +0100

    tailwind fixes

--- inzynierka/src/frontend/Components/Sidebar.js
+++ inzynierka/src/frontend/Components/Sidebar.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
-import '../../css/sidebar.css';
 import leaf from './leaf.png';
 
 const Sidebar = ({ isOpen, toggleSidebar }) => {
@@ -21,38 +20,38 @@ const Sidebar = ({ isOpen, toggleSidebar }) => {
     };
 
     return (
-        <div className={`block fixed w-0 z-50 overflow-x-hidden h-full left-0 sidebar bg-[#8ac598] ${isOpen ? 'w-[300px]' : ''}`}>
-            <div className='flex mt-[20px] justify-between pr-[10px] items-center'>
+        <div className={`shadow-[5px_0_15px_rgba(0,0,0,0.3)] block fixed w-0 z-50 overflow-x-hidden h-full left-0 transition-all duration-500 ease-in-out bg-[#8ac598] ${isOpen ? 'w-[300px]' : ''} flex flex-col`}>
+            <div className='flex mt-[20px] justify-between pr-[40px] items-center'>
                 <p className='w-[60px] h-[60px]'></p>
-                <p onClick={goToHome}><img src={leaf} alt='Earth' className='w-[60px] h-[60px]' /></p>
+                <p onClick={goToHome}><img src={leaf} alt='Earth' className='w-[50px] h-[50px]' /></p>
                 <button className="w-[40px] h-[40px] bg-[#5ca86e] hover:bg-[#409A55] text-white rounded" onClick={toggleSidebar}>X</button>
             </div>
 
             <nav className='text-center mt-[100px]'>
-                <ul className='[&>li]:w-[200px] [&>li]:mt-[10px] justify-items-center'>
-                    <li className={` Overwiev ${location.pathname === '/UserAcc' ? 'bg-[#5ca86e] hover:bg-[#409A55]' : ''}`}onClick={() => goToPage('/UserAcc')}>
+                <ul className='[&>li]:cursor-pointer [&>li]:w-[200px] [&>li]:mt-[10px] justify-items-center [&>li]:text-white [&>li]:rounded [&>li]:p-[10px] [&>li]:font-bold'>
+                    <li className={`${location.pathname === '/UserAcc' ? 'bg-[#5ca86e] hover:bg-[#409A55]' : 'hover:bg-[#409A55]'}`} onClick={() => goToPage('/UserAcc')}>
                         Overview
                     </li>
-                    <li className={`Statistics ${location.pathname === '/Statistics' ? 'bg-[#5ca86e] hover:bg-[#409A55]' : ''}`}onClick={() => goToPage('/Statistics')}>
+                    <li className={`${location.pathname === '/Statistics' ? 'bg-[#5ca86e] hover:bg-[#409A55]' : 'hover:bg-[#409A55]'}`} onClick={() => goToPage('/Statistics')}>
                         Statistics
                     </li>
-                    <li className={`Rankings ${location.pathname === '/Rankings' ? 'bg-[#5ca86e] hover:bg-[#409A55]' : ''}`}onClick={() => goToPage('/Rankings')}>
+                    <li className={`${location.pathname === '/Rankings' ? 'bg-[#5ca86e] hover:bg-[#409A55]' : 'hover:bg-[#409A55]'}`} onClick={() => goToPage('/Rankings')}>
                         Rankings
                     </li>
-                    <li className={`Trophies ${location.pathname === '/Trophies' ? 'bg-[#5ca86e] hover:bg-[#409A55]' : ''}`}onClick={() => goToPage('/Trophies')}>
+                    <li className={`${location.pathname === '/Trophies' ? 'bg-[#5ca86e] hover:bg-[#409A55]' : 'hover:bg-[#409A55]'}`} onClick={() => goToPage('/Trophies')}>
                         Trophies
                     </li>
-                    <li className={`Calendar ${location.pathname === '/Calendar' ? 'bg-[#5ca86e] hover:bg-[#409A55]' : ''}`}onClick={() => goToPage('/Calendar')}>
+                    <li className={`${location.pathname === '/Calendar' ? 'bg-[#5ca86e] hover:bg-[#409A55]' : 'hover:bg-[#409A55]'}`} onClick={() => goToPage('/Calendar')}>
                         Calendar
                     </li>
-                    <li className={`Settings ${location.pathname === '/Settings' ? 'bg-[#5ca86e] hover:bg-[#409A55]' : ''}`}onClick={() => goToPage('/Settings')}>
+                    <li className={`${location.pathname === '/Settings' ? 'bg-[#5ca86e] hover:bg-[#409A55]' : 'hover:bg-[#409A55]'}`} onClick={() => goToPage('/Settings')}>
                         Settings
-                    </li>
-                    <li className="logout" onClick={() => handleLogout('/')}>
-                        <a href=".">Logout</a>
                     </li>
                 </ul>
             </nav>
+            <div className="self-center rounded w-[200px] p-[10px] hover:bg-[#409A55] flex justify-center mt-auto text-white font-bold mb-[50px]" onClick={() => handleLogout('/')}>
+                <a href=".">Logout</a>
+            </div>
         </div>
     );
 };
--- inzynierka/src/frontend/Components/SidebarAdmin.js
+++ inzynierka/src/frontend/Components/SidebarAdmin.js
@@ -1,31 +1,35 @@
 import React, { useEffect, useState } from 'react';
-import { useNavigate, useLocation } from 'react-router-dom';
-import '../../css/sidebar.css';
+import { useNavigate } from 'react-router-dom';
 import leaf from './leaf.png';
 
-const SidebarAdmin = ({isOpen, toggleSidebar, toggleModal}) => {
+const SidebarAdmin = ({ isOpen, toggleSidebar, toggleModal }) => {
     const navigate = useNavigate();
-    const location = useLocation();
     const [isFirstLoad, setIsFirstLoad] = useState(true);
+    const [Current, setCurrent] = useState('');
 
     useEffect(() => {
         if (isFirstLoad) {
             toggleModal('overview');
-            setIsFirstLoad(false); 
+            setCurrent('overview');
+            setIsFirstLoad(false);
         }
     }, [isFirstLoad, toggleModal]);
 
     const showOverview = () => {
         toggleModal('overview');
+        setCurrent('overview');
     };
     const showUsers = () => {
         toggleModal('users');
+        setCurrent('users');
     };
     const showEvents = () => {
         toggleModal('events');
+        setCurrent('events');
     };
     const showNotifications = () => {
         toggleModal('notifications');
+        setCurrent('notifications');
     };
     const handleLogout = () => {
         localStorage.removeItem('authToken');
@@ -34,24 +38,33 @@ const SidebarAdmin = ({isOpen, toggleSidebar, toggleModal}) => {
         navigate('/');
     };
 
-    const sidebarClass = `sidebar ${isOpen ? 'open' : ''}`;
-
     return (
-        <div className={sidebarClass}>
-            <div className='row centerImg'>
-                <p  onClick={showOverview}><img src={leaf} alt='Earth' className='leaf-image inline' /></p>
-                <button className="close-btn" onClick={toggleSidebar}>X</button>
+        <div className={`shadow-[5px_0_15px_rgba(0,0,0,0.3)] block fixed w-0 z-50 overflow-x-hidden h-full left-0 transition-all duration-500 ease-in-out bg-[#8ac598] ${isOpen ? 'w-[300px]' : ''} flex flex-col`}>
+            <div className='flex mt-[20px] justify-between pr-[40px] items-center'>
+                <p className='w-[60px] h-[60px]'></p>
+                <p onClick={showOverview}><img src={leaf} alt='Earth' className='w-[50px] h-[50px]' /></p>
+                <button className="w-[40px] h-[40px] bg-[#5ca86e] hover:bg-[#409A55] text-white rounded" onClick={toggleSidebar}>X</button>
             </div>
-            
-            <nav className='navCenter'>
-                <ul>
-                    <li className='T Home' onClick={showOverview}>Overview</li>
-                    <li className='T Statistics' onClick={showUsers}>Users</li>
-                    <li className='T Rankings' onClick={showEvents}>Events</li>
-                    <li className='T Trophies' onClick={showNotifications}>Notifictions</li>
-                    <li className="logout" onClick={handleLogout}><a href=".">Logout</a></li>
+
+            <nav className='text-center mt-[100px]'>
+                <ul className='[&>li]:cursor-pointer [&>li]:w-[200px] [&>li]:mt-[10px] justify-items-center [&>li]:text-white [&>li]:rounded [&>li]:p-[10px] [&>li]:font-bold'>
+                    <li className={`${Current === 'overview' ? 'bg-[#5ca86e] hover:bg-[#409A55]' : 'hover:bg-[#409A55]'}`} onClick={showOverview}>
+                        Overview
+                    </li>
+                    <li className={`${Current === 'users' ? 'bg-[#5ca86e] hover:bg-[#409A55]' : 'hover:bg-[#409A55]'}`} onClick={showUsers}>
+                        Users
+                    </li>
+                    <li className={`${Current === 'events' ? 'bg-[#5ca86e] hover:bg-[#409A55]' : 'hover:bg-[#409A55]'}`} onClick={showEvents}>
+                        Events
+                    </li>
+                    <li className={`${Current === 'notifications' ? 'bg-[#5ca86e] hover:bg-[#409A55]' : 'hover:bg-[#409A55]'}`} onClick={showNotifications}>
+                        Notifictions
+                    </li>
                 </ul>
             </nav>
+            <div className="self-center rounded w-[200px] p-[10px] hover:bg-[#409A55] flex justify-center mt-auto text-white font-bold mb-[50px]" onClick={handleLogout}>
+                <a href=".">Logout</a>
+            </div>
         </div>
     );
 };
