a2f947036f55da4814f9a8c507f62884086ba57c
Julia Kapica <jkapica@edu.cdv.pl>
2024-12-16 23:29:03 +0100

    register errors/progress bar percentage fixed:D

--- inzynierka/src/frontend/Register.js
+++ inzynierka/src/frontend/Register.js
@@ -19,10 +19,10 @@ const Register = () => {
     const validateName = (name) => /^[A-Za-z]{2,30}$/.test(name);
     const validatePassword = (password) => {
         if (password.length < 8) {
-            return 'Min 8 chars';
+            return 'Min. 8 characters';
         }
         if (password.length > 24) {
-            return 'Max 24 chars';
+            return 'Max. 24 characters';
         }
         if (!/[a-z]/.test(password)) {
             return 'At least 1 lowercase';
@@ -34,7 +34,7 @@ const Register = () => {
             return 'At least 1 digit';
         }
         if (!/[\W_]/.test(password)) {
-            return 'At least 1 special char';
+            return 'At least 1 special character';
         }
         if (/\s/.test(password)) {
             return 'No whitespace';
@@ -88,7 +88,7 @@ const Register = () => {
         }
 
         if (!validateGender(gender)) {
-            setGenderError('Invalid gender');
+            setGenderError('Select gender');
             isValid = false;
         } else {
             setGenderError('');
@@ -115,7 +115,7 @@ const Register = () => {
                 if (!response.ok) {
                     return response.json().then(errorData => {
                         if (errorData.error === 'username exists') {
-                            setErrorMessageDuplicate('Nickname is already in use');
+                            setErrorMessageDuplicate('Username is already in use');
                         }
                         else if (errorData.error === 'email exists') {
                             setErrorMessageDuplicate('Email is already in use');
@@ -230,9 +230,14 @@ const Register = () => {
                     </div>
                     <div className='flex items-center justify-between'>
                         <button
-                            className='w-full max-w-[120px] p-2 font-medium text-[#6E9B7B] '
+                            className='w-full max-w-[160px] p-2 font-medium text-xl text-[#6E9B7B] flex gap-[10px] items-center'
                             onClick={() => window.location.href = '../'}
                         >
+                            <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 22 22" fill="none">
+                                <path d="M12.5 8L9.5 11L12.5 14" stroke="#6E9B7B" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                                <path d="M1 11C1 6.286 1 3.929 2.464 2.464C3.93 1 6.286 1 11 1C15.714 1 18.071 1 19.535 2.464C21 3.93 21 6.286 21 11C21 15.714 21 18.071 19.535 19.535C18.072 21 15.714 21 11 21C6.286 21 3.929 21 2.464 19.535C1 18.072 1 15.714 1 11Z" stroke="#6E9B7B" stroke-width="1.5"/>
+                            </svg>
+
                             Go back
                         </button>
                         <button
